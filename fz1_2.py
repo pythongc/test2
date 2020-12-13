e6bff36f371e178caf0bddb582ebc56cdbd965e3
pythongc <578763285@qq.com>
2020-12-13 09:32:41 +0800

    test1

---
+++ fz1_2.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+#__author:gc
+#date:2020/12/13
+print(1_2)
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(11)
+print('分支1_2')
+
+def test():
+    print(11111)
+
+
+if __name__ == '__mian':
+    pass
+
+
